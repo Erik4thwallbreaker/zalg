8920349361e6c8e5b4908a66ed7e391b065c70b0
Erik <erik@Knuts-MBP.home>
2024-04-03 13:59:52 +0200

    Minor change to executing code in unittest.py

--- unittests.py
+++ unittests.py
@@ -3,5 +3,5 @@ from unipolynomial import *
 from zfield import *
 
 # Doing some things
-DumbArray = [[0 for i in range(3)] for i in range(3)]
+MyArray = [[0 for i in range(3)] for i in range(3)]
 print(DumbArray)
