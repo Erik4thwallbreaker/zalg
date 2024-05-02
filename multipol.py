4449954541864f47acadbe6979020eb9e194594e
Erik <erik@Knuts-MBP.home>
2024-05-02 10:34:26 +0200

    Minor preliminary fixes

--- multipol.py
+++ multipol.py
@@ -1,10 +1,21 @@
 #Free commutative monoid: A class with just enough structure to allow free polynomial ring mulitlpication.
 class Fcm:																
-	def __init__( self, indices = (), indeterms = () ):												#TODO ?Should the tuples always be reduced to avoid duplicates, or be standardised?
-		self.indices = indices																		#Int - the order with respect to each indeterminate.
-		self.indeterms = indeterms																	#Can be strings, or more esoteric ring objects. MUST BE EQUAL LENGTH. Assumed to all be distinct.
+	@staticmethod																					#Abelian monoid operation. Adds indexes together without creating duplicates, (potentially zeroes)
+	def abela(lefthand, righthand):																	#TODO Fix so that it doesnt alter the lefthand input
+		for key,value in righthand.items():
+			if key in lefthand:
+				lefthand[key] += righthand[key]
+			else:
+				lefthand[key] = righthand[key]
+		return lefthand
 
-	def __iter__(self):
+
+	def __init__( self, indices = (), indeterms = () ):
+		pro_dict = {key: value for key, value in zip(indeterms, indices) if value != 0}
+		self.indeterms = tuple(pro_dict.keys())														#Can be strings for formal multiplication or other ring objects.
+		self.indices = tuple(pro_dict.values())														#Int - the order with respect to each indeterminate.
+
+	def __iter__(self):																				#TODO Can it be made so that it cooperates with dict()?
 		self.counter = 0
 		return self
 
@@ -12,10 +23,10 @@ class Fcm:
 		if self.counter >= len(self.indices):
 			raise StopIteration
 		self.counter += 1
-		return self.indices[self.counter - 1]
+		return (self.indeterms[self.counter -1], self.indices[self.counter - 1])
 
 	def __eq__(self, other):
-		return dict(zip(self.indeterms, self.indices)) == dict(zip(other.indeterms, other.indices))	#TODO Should add a condition for 0 exponent or add a reduction.
+		return dict(zip(self.indeterms, self.indices)) == dict(zip(other.indeterms, other.indices))
 
 	def __ne__(self, other):
 		return not(self == other)
@@ -24,7 +35,10 @@ class Fcm:
 		return hash(frozenset(zip(self.indices, self.indeterms)))
 
 	def __mul__( self, other):																		#Assumes the indices refer to the same indeterminates and therefore have equal length.
-		return tuple(( sum(i) for i in zip(self.indices, other.indices) ))							#TODO Is outdated. Need fixing to work with different sequences of indetermiantes.
+		lefthand = dict(zip(self.indeterms, self.indices))
+		righthand = dict(zip(self.indeterms, self.indices))
+		pro_dict = Fcm.abela(lefthand, righthand)
+		return Fcm( indeterms = tuple(pro_dict.keys()), indices = tuple(pro_dict.values()) )
 
 	def __str__(self):																				#Has its own structure for the string expression of a single product/power.
 		all_factors = ['^'.join(i) for i in zip(map(str, self.indeterms), map(str, self.indices)) ]	#TODO Make nicer. With better list comprehension.
@@ -49,7 +63,7 @@ class Multipolynomial:
 		else:																						#Adds numeric types as constant polynomials. TODO Adds string types through the string interpreter.
 			lefthand = dict(self.terms)
 			righthand = {Fcm(): other}															
-		for key, value in righthand.items():
+		for key, value in righthand.items():														#TODO Fix
 			for key, value in righthand.items():
 				if key in lefthand:
 					lefthand[key] += righthand[key]
@@ -57,16 +71,3 @@ class Multipolynomial:
 					lefthand[key] = righthand[key]
 		return Multipolynomial(lefthand)
 
-
-
-
-myc = ('x','y')
-coefs1 = {Fcm((2,0), myc):1, Fcm((1,1), myc):2, Fcm((0,2), myc):1}
-coefs2 = {Fcm((2,0), myc):4}
-pol1 = Multipolynomial(coefs1)
-pol2 = Multipolynomial(coefs2)
-pol3 = pol1 + pol2
-print(pol1)
-print(pol2)
-print(pol3)
-print( Fcm((1,0),('x','y')) == Fcm((1,),('x',)) )
